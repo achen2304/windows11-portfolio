7b1741cc3ff3d35277ffa8ed04db51612704d397
Cai Chen <achen2304@gmail.com>
2026-02-14 17:15:59 -0600

    chore: update project and experience data, enhance about messages, and standardize code formatting

--- src/data/experience.tsx
+++ src/data/experience.tsx
@@ -1,30 +1,24 @@
 // My professional experiences: use google to research more about the company if asked.
 export const EXPERIENCES = [
   {
-    company: 'Todd Agriscience',
-    companyUrl: 'https://toddagriscience.com',
-    position: 'Associate Software Engineer',
-    period: 'Aug 2025 - Present',
-    description: [
-      'Spearheading the development of a new web application for Todd Agriscience, leveraging modern web technologies and best practices to create a seamless and efficient user experience.',
-    ],
+    company: "Todd Agriscience",
+    companyUrl: "https://toddagriscience.com",
+    position: "Associate Software Engineer",
+    period: "Aug 2025 - Present",
+    description: ["Spearheading the development of a new web application for Todd Agriscience, leveraging modern web technologies and best practices to create a seamless and efficient user experience."],
   },
   {
-    company: 'Dwolla',
-    companyUrl: 'https://dwolla.com',
-    position: 'Software Engineering Intern',
-    period: 'May 2025 - Aug 2025',
-    description: [
-      'Worked on the Dwolla Dashboard team, migrating the dashboard to a new tech stack using React, Next.js, MUI, and Redux Toolkit, along with taking on more difficult tickets involving the backend in scala, the old dashboard in Hapi.js, and more.',
-    ],
+    company: "Dwolla",
+    companyUrl: "https://dwolla.com",
+    position: "Software Engineering Intern",
+    period: "May 2025 - Aug 2025",
+    description: ["Worked on the Dwolla Dashboard team, migrating the dashboard to a new tech stack using React, Next.js, MUI, and Redux Toolkit, along with taking on more difficult tickets involving the backend in scala, the old dashboard in Hapi.js, and more."],
   },
   {
-    company: 'Iowa State University',
-    companyUrl: 'https://iastate.edu',
-    position: 'Student',
-    period: 'Aug 2023 - Dec 2026',
-    description: [
-      'Student at Iowa State University, pursuing a Bachelor of Science in Computer Engineering.',
-    ],
+    company: "Iowa State University",
+    companyUrl: "https://iastate.edu",
+    position: "Student",
+    period: "Aug 2023 - Dec 2026",
+    description: ["Student at Iowa State University, pursuing a Bachelor of Science in Computer Engineering."],
   },
 ];
